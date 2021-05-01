361eab2f43fb1e0b9101b86aa7b1107940fe0ea2
RIDDHI <riddhi.gupta2019@vitstudent.ac.in>
2021-05-01 23:20:06 +0530

    login added

---
+++ Phraze/ConvertViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ConvertViewController.swift
+//  Phraze
+//
+//  Created by riddhi gupta on 01/05/21.
+//
+
+import UIKit
+
+class ConvertViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let imageName = "Landing-Illust"
+        let image = UIImage(named: imageName)
+        let imageView = UIImageView(image: image!)
+        imageView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        imageView.contentMode = .scaleAspectFill
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
